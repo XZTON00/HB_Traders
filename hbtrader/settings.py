3f865f714f76d2f6dc3ddcaaf11fca3af7d20e67
Arslan <devil99opa@gmail.com>
2026-09-25 19:51:13 +0530

    Allow Railway host

--- hbtrader/settings.py
+++ hbtrader/settings.py
@@ -26,8 +26,11 @@ SECRET_KEY = config(
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
-
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='localhost,127.0.0.1,hbtraders-production.up.railway.app',
+    cast=Csv(),
+)
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='', cast=Csv())
 
 
